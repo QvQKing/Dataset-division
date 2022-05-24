1922aeea66f25fe3fc748211e543a7fcd1a67d51
king-king-king <52349759+king-king-king@users.noreply.github.com>
2022-05-24 22:11:08 +0800

    Update Multi classification.py

--- Multi classification.py
+++ Multi classification.py
@@ -1,19 +1,19 @@
 import os, random, shutil
-
-
-def moveFile(fileDir, tarDir):
+def moveFile(fileDir):
     pathDir = os.listdir(fileDir)  # 取图片的原始路径
     filenumber = len(pathDir)
-    rate = 0.1  # 自定义抽取图片的比例，比方说100张抽10张，那就是0.1
-    picknumber = int(filenumber * rate)  # 按照rate比例从文件夹中取一定数量图片
+    picknumber = int(filenumber * ratio)  # 按照rate比例从文件夹中取一定数量图片
     sample = random.sample(pathDir, picknumber)  # 随机选取picknumber数量的样本图片
-    print(sample)
     for name in sample:
-        shutil.move(fileDir + name, tarDir + "\\" + name)
+        shutil.move(os.path.join(fileDir, name), os.path.join(tarDir, name))
     return
-
 if __name__ == '__main__':
-    fileDir = r"D:\snoring-dataset\Snoring Dataset\图像数据\mfcc-dataset\train\ss/" # 源图片文件夹路径
-    tarDir = r'D:\snoring-dataset\Snoring Dataset\图像数据\mfcc-dataset\test\ss/'  # 移动到新的文件夹路径
-    moveFile(fileDir, tarDir)
-
+    ori_path = 'D:/snoring-dataset/Snoring Dataset/309-no-snoring-1'  # 最开始train的文件夹路径
+    split_Dir = 'D:/snoring-dataset/Snoring Dataset/test-309'  # 移动到新的文件夹路径
+    ratio = 0.1  # 抽取比例
+    for firstPath in os.listdir(ori_path):
+        fileDir = os.path.join(ori_path, firstPath)  # 原图片文件夹路径
+        tarDir = os.path.join(split_Dir, firstPath)  # val下子文件夹名字
+        if not os.path.exists(tarDir):  # 如果val下没有子文件夹，就创建
+            os.makedirs(tarDir)
+        moveFile(fileDir)  # 从每个子类别开始逐个划分
